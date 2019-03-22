bd22d82d41d0c96b36fe484a0b07b7235f0da52f
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-03-22 15:36:00 -0400

    Fetching finally working, needed help with roughly all of it

--- src/App.js
+++ src/App.js
@@ -1,48 +1,58 @@
 import React, { Component } from 'react';
-import Welcome from 'Welcome';
-import PlanetList from 'PlanetList';
-import RandomPlanet from 'RandomPlanet';
-import PlanetDetail from 'PlanetDetail';
+import { Route, Link } from "react-router-dom";
 import './App.css';
+
+import Welcome from './Welcome';
+import PlanetList from './PlanetList';
+import RandomPlanet from './RandomPlanet';
+import PlanetDetail from './PlanetDetail';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state={
-
-      planetList:[],
-
+    this.state = {
+      
+      planetList: [],
+      
     }
-    this.getPlanetNames=this.getPlanetNames.bind(this)
-    this.listAll=this.listAll.bind(this)
+    this.getPlanetNames = this.getPlanetNames.bind(this)
   }
-componentDidMount() {
-  this.getPlanetNames()
-}
+  componentDidMount() {
+    this.getPlanetNames()
+  }
+  
+  getPlanetNames() {
+    fetch('https://wdi-nyc-planets-api.herokuapp.com/planets')
+    .then(response =>  response.json())
+    .then(planetData => {
+      this.setState({ 
+        list: planetData.planets 
+      })
+        console.log(planetData)
+    })
+  }
 
-getPlanetNames() {
-  fetch(`https://wdi-nyc-planets-api.herokuapp.com/planets`)
-  .then(response => {
-    return response.json()
-  })
-  .then(jsonData => {
-    return this.setState({planetList: jsonData})
-  })
-}
 
-listAll() {
-  list.state.planetList.map(planet => <div><p>{planet[0]}</p></div>)
-}
 
 
 
   render() {
     return (
       <div className="App">
-        <Welcome />
-        <PlanetList />
-        <RandomPlanet />
-        <PlanetDetail />
+        <nav>
+          <ul>
+            <li><Link to="/">Welcome</Link></li>
+            <li><Link to="/planetList">PlanetList</Link></li>
+            <li><Link to="/randomPlanet">RandomPlanet</Link></li>
+            <li><Link to="/planetDetail">PlanetDetail</Link></li>
+          </ul>
+        </nav>
+        <div className="main">
+          <Route exact path="/" component={Welcome} />
+          <Route path="/planetList" component={PlanetList} />
+          <Route path="randomPlanet" component={RandomPlanet} />
+          <Route path="planetDetail" component={PlanetDetail} />
+        </div>
       </div>
     );
   }
